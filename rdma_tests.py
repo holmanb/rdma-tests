b006bea60a9525e003564fe912c3f8173a573b6b
Brett Holman <holmanb0214@my.uwstout.edu>
2018-02-15 12:53:43 -0800

    added some basic argument parsing

--- rdma_tests.py
+++ rdma_tests.py
@@ -1,21 +1,23 @@
+#!/usr/bin/python
 import argparse
 import logging
 
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-g","--group",help="Specify group of tests to run")
-    parser.add_argument("-t","--test",help="Specify individual tests to run")
-    
+    parser.add_argument("-g","--group",help="Specify comma delimited groups of tests to run")
+    parser.add_argument("-t","--test",help="Specify comma delimited list of individual tests to run")
 
 
-    print("Hello world")
     args = parser.parse_args()
+
     if not args.group and not args.test:
-        print("Running all of the test")
+        print("Running all of the tests")
+    if args.group:
+        print("Running tests in groups: {}".format(args.group))
+    if args.test:
+        print("Running tests: {}".format(args.test))
 
-
-    print(args.group) 
 
 
 
