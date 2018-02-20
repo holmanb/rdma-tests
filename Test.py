1be83d50c86c2c12e5201b4b7b99e0c66013a3c8
Brett Holman <holmanb0214@my.uwstout.edu>
2018-02-20 10:29:26 -0800

    added another test to Test

--- Test.py
+++ Test.py
@@ -3,7 +3,7 @@
 
 class Test:
 
-    def __init__(self, name, description, script, args):
+    def __init__(self, name=None, description=None, script=None, args=None):
         """ Used for creating a common interface for all tests.
         """
 
@@ -18,9 +18,21 @@ class Test:
     def get_name(self):
         return self._name
 
+    def set_name(self, name):
+        self._name = name
+
+    def get_description(self):
+        return self._description
+
+    def set_description(self, description):
+        self._description = description
+
     def get_description(self):
         return self._description
 
 if __name__ == "__main__":
     testing_the_test = Test("MyName", "My name is a simple test", lambda x:x*2, 5)
+    description = "quick description"
+    testing_the_test.set_description(description)
     assert testing_the_test.run() == 10, "Test class is broken" 
+    assert testing_the_test.get_description() == description, "Test class is broken"
