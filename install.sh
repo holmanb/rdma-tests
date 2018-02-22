61d74254a9448378634c06b039267bf972b2ca7e
Brett Holman <holmanb0214@my.uwstout.edu>
2018-02-22 12:41:36 -0600

    renamed and changed directory structure, added paramiko install to install script

--- install.sh
+++ install.sh
@@ -5,7 +5,11 @@ yum install python34 -y
 # Pip and paramiko
 curl -O https://bootstrap.pypa.io/get-pip.py
 /usr/bin/python3.4 get-pip.py
+mv gig-pip.py /tmp
 
+# Get paramiko dependencies
 yum install gcc libffi-devel python-devel openssl-devel -y
 
+# install paramiko
+pip install paramiko
 
