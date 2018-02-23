f6509ebacf21bbfdcc2bd9e1d573fcb268d4757b
Brett Holman <holmanb0214@my.uwstout.edu>
2018-02-22 19:01:41 -0600

    added group attribute and associated methods

--- interop/testlib/testclass.py
+++ interop/testlib/testclass.py
@@ -3,7 +3,7 @@
 
 class Test:
 
-    def __init__(self, name=None, description=None, script=None, args=None):
+    def __init__(self, name=None, description=None, script=None, args=None, group=None):
         """ Creates a common interface for all tests.
         """
 
@@ -11,6 +11,7 @@ class Test:
         self._description = description
         self._script = script
         self._args = args
+        self._group=group or []
 
     def __lt__(self, other):
         """ Allows Tests to be sorted.  Default sorting should be by name.
@@ -43,10 +44,30 @@ class Test:
     def get_description(self):
         return self._description
 
+    def add_group(self, group):
+        """ accepts a string as a group, or a list of groups
+        """
+
+        # Add single group
+        if isinstance(group, str):
+            self._group.append(group)
+
+        # Add a list/of groups
+        elif isinstance(group, list):
+            self._group += group
+        else:
+            raise TypeError
+
+    def get_groups(self):
+        return self._group
+
 if __name__ == "__main__":
     testing_the_test = Test("MyName", "My name is a simple test", lambda x:x*2, 5)
     description = "quick description"
     testing_the_test.set_description(description)
     assert testing_the_test.run() == 10, "Test class is broken" 
     assert testing_the_test.get_description() == description, "Test class is broken"
-
+    assert testing_the_test.get_groups() == [], "Test class is broken"
+    assert Test(group="group2").get_groups()=="group2", "Test class groups is broken"
+    testing_the_test.add_group("group1")
+    assert testing_the_test.get_groups()[0] == "group1", "Test class groups is broken"
