73a2bb33edc3cd723c2f1e45d355bde8a5a74fc8
Brett Holman <holmanb0214@my.uwstout.edu>
2018-02-27 13:14:30 -0700

    modified installer to get python34 tools rather than python2 tools

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ yum install epel-release -y
 yum install python34 -y
 
 # Pip and python development package
-yum install python-pip
-yum install python-devel
+yum install python34-pip -y
+yum install python34-devel -y
 
 # Get non-python paramiko dependencies
 yum install gcc libffi-devel openssl-devel -y
 
 # install paramiko
-pip install paramiko
+pip3 install paramiko
 
