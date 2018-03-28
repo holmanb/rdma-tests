5fbbd645e1238e5b130671843780dee4a84a5201
Brett Holman <holmanb0214@my.uwstout.edu>
2018-03-28 12:52:18 -0600

    closes #15 and moves our sample test location into scripts

--- interop/testlib/scripts/infiniband/sample_tests.py
+++ interop/testlib/scripts/infiniband/sample_tests.py
@@ -1,7 +1,6 @@
 import testlib.test as Test
 
 
-
 def sample_test1():
 
     print( "this is running sample_test1 that is imported from /infiniband/sample_tests.py")
@@ -12,9 +11,8 @@ def sample_test2():
     print( "this is running sample_test2 that is imported from /infiniband/sample_tests.py")
 
 
-sampleTest1 = Test.Test(script=sample_test1, name="sample1", description="this is the sample_test1 description", group=["latency_tests","bandwidth_tests"])
+sampleTest1 = Test.Test(script=sample_test1,  description="this is the sample_test1 description", group=["latency_tests","bandwidth_tests"])
 
 sampleTest2 = Test.Test(script=sample_test2)
 sampleTest2.set_description("this is the sample_test2 description")
-sampleTest2.set_name("sample2")
 sampleTest2.add_group("latency_tests")
