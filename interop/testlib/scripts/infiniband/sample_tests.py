be49c12afdf6bee15647853c27ddd8ee01be95cd
Aaron Overby <overbya0300@my.uwstout.edu>
2018-04-10 13:18:23 -0600

    Changed test 11.2 to go through all nodes that areactive

--- interop/testlib/scripts/infiniband/sample_tests.py
+++ interop/testlib/scripts/infiniband/sample_tests.py
@@ -3,38 +3,49 @@ import testlib.classes.network as network
 import time
 
 def IBFabricInit():
-    if(network.nodes[0].sm.status() == "inactive"):
-        network.nodes[0].sm.start()
-    #Generating a topology file
-    network.nodes[0].command("ibdiagnet -wt 'topology'")
-    #Clearing all port counters
-    network.nodes[0].command("ibdiagnet -pc")
-    #Waiting for 17 seconds per the specification requirements
-    time.sleep(17)
-    #Sending 1000 node descriptions
-    network.nodes[0].command("ibdiagnet -c 1000")
-    #Generating fabric report, use /tmp/ibdiagnet.sm file to see running s
-    network.nodes[0].command("ibdiagnet")
-    #Skipping the building of GUID list for now
-    #Comparing current topology against topology file made at beginning of test
-    Stdout = network.nodes[0].command("ibdiagnet -t 'topology' -s 'S248a0703002a02dc'")
-    print(Stdout)
-    string = "perfectly matches the discovered fabric"
-    string2 = "No bad Guids were found"
-    string3 = "No illegal PM counters values were found"
+    for node in network.nodes:
+        if node.isup():
+            if(node.sm.status() == "inactive"):
+                node.sm.start()
+            ibstat = node.command("ibstat").split("\n")
+            # Getting the system name, then putting it into format that ibdiagnet wants
+            # from 0x<systemguid> to S<systemguid>
+            for lines in ibstat:
+                if "System image GUID:" in lines:
+                    stat2 = lines.split()
+                    systemGUID = "S" + stat2[-1][2:]
 
-    f = open('/var/cache/ibutils/ibdiagnet.log', 'r')
-    contents = f.readlines()
-    for line in contents:
-        if string in line:
-            print("Topology from before and after test matches")
-        if string2 in line:
-            print("No illegal PM counters")
-        if string3 in line:
-            print("No bad Guids found")
+            #Generating a topology file
+            node.command("ibdiagnet -wt 'topology'")
+            #Clearing all port counters
+            node.command("ibdiagnet -pc")
+            #Waiting for 17 seconds per the specification requirements
+            time.sleep(17)
+            #Sending 1000 node descriptions
+            node.command("ibdiagnet -c 1000")
+            #Generating fabric report, use /tmp/ibdiagnet.sm file to see running s
+            node.command("ibdiagnet")
+            #Skipping the building of GUID list for now
+            #Comparing current topology against topology file made at beginning of test
+            Stdout = node.command("ibdiagnet -t 'topology' -s '{}'".format(systemGUID))
+            print(Stdout)
+            string = "perfectly matches the discovered fabric"
+            string2 = "No bad Guids were found"
+            string3 = "No illegal PM counters values were found"
 
-    f.close()
+            f = open('/var/cache/ibutils/ibdiagnet.log', 'r')
+            contents = f.readlines()
+            for line in contents:
+                if string in line:
+                    print("Topology from before and after test matches")
+                if string2 in line:
+                    print("No illegal PM counters")
+                if string3 in line:
+                    print("No bad Guids found")
 
+            f.close()
+        else:
+            print("{} is not currently active, running tests on other active nodes".format(node.ethif.id)
 
 def sample_test1():
 
