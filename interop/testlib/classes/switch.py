c09b17aed3c2f17ced5eccdd93732f1f34ab4281
Brett Holman <holmanb0214@my.uwstout.edu>
2018-04-19 12:01:01 -0600

    much closer to having a switch.reboot() commmand, also fixed the returned output for the master node's command() function

--- interop/testlib/classes/switch.py
+++ interop/testlib/classes/switch.py
@@ -10,13 +10,22 @@ except Exception as e:
 
 import sys
 
+class SwitchInitializationError(Exception):
+    """ Provides error if incorrect initialization occurs
+    """
+    pass
+
 class Switch(Node):
-    def __init__(self, ethif=None, available=None, username=None):
-        """ Parent has side effects we don't wand
+    def __init__(self, ethif=None, available=None, username=None, switch_type=None):
+        """ Parent has side effects we don't want
         """
         self.ethif=ethif
-        self._available=None
+        self._available=available
         self.username=username
+        self.type = switch_type
+
+        if (not ethif) or (not username) or (not switch_type):
+            raise SwitchInitializationError("Need to provide ethif, username and type")
 
     def print(self):
         """ Prints switch information
@@ -30,4 +39,28 @@ class Switch(Node):
     def whoami(self):
         """ Example of using inherited method.
         """
-        super().command('whoami',username=self.username)
+        return super().command('whoami',username=self.username)
+
+    def reboot(self):
+        """ Reboots the switch according to the appropriate commands
+        """
+        def mlnx():
+            """ For rebooting switches that use mlnx-os
+            """
+            pass
+
+        def nix_like():
+            """ For rebooting switches that use *nix-like shutdown commands
+            """
+            pass
+
+        if self.type == 'mlnx':
+            mlnx()
+        elif self.type == 'cumulus': 
+            nix_like()
+            
+
+    def write_file(self):
+        """ Example of using inherited method.
+        """
+        super().command('echo mytext > tmpfile',username=self.username)
