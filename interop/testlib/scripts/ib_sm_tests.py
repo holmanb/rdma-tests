6a2c1ddd5ac27592834b5555c25bf394a7fd10c3
Kevin Pelzel <kevinpelzel@Kevins-MBP.eau.wi.charter.com>
2018-04-26 13:32:27 -0500

    outputting sminfo if it fails

--- interop/testlib/scripts/ib_sm_tests.py
+++ interop/testlib/scripts/ib_sm_tests.py
@@ -136,11 +136,11 @@ def nodePairs(node1, node2, guid_list):
     else:
         print("Nodes were successfully verified and existing")
 
+    print("running sminfo on {} to check if it's master".format(node1.ethif.aliases[0]))
     # using sminfo, verify that the running SM is the master
     sminfo_output = node1.command("sudo sminfo -L {}".format(node1_lid))
-    print(sminfo_output[0])
     if "SMINFO_MASTER" not in sminfo_output[0]:
-        return [False, "Node1({}) is not reporting to be the master node".format(node1.ethif.aliases[0])]
+        return [False, "Node1({}) is not reporting to be the master node. Output from sminfo: {}".format(node1.ethif.aliases[0],sminfo_output)]
     else:
         print("Node1 ({}) correctly reported being the master node".format(node1.ethif.aliases[0]))
 
