50a6f41654bc2a173fb12a419f6ca4c650330254
Kevin Pelzel <kevinpelzel@Kevins-MacBook-Pro.local>
2018-04-30 12:42:19 -0500

    finished writing test 4

--- interop/testlib/scripts/ib_sm_tests.py
+++ interop/testlib/scripts/ib_sm_tests.py
@@ -290,7 +290,7 @@ def test3(node1, node2, guid_list):
         node2_lid = node_temp_lid
         node2_name = node_temp_name
     else:
-        return [False, "Test 3 error determining which node is master:\n{} state = {}\n{} state = {}".format(node1_name, node1_state, node2_name, node2_state)]
+        return [False, "Error determining which node is master:\n{} state = {}\n{} state = {}".format(node1_name, node1_state, node2_name, node2_state)]
 
     
     # Shutdown the master SM.
@@ -300,16 +300,18 @@ def test3(node1, node2, guid_list):
     time.sleep(3)
     #Verify SM is stopped
     if node1.sm.status() == 'active':
-        return [False, "Step7: Node 1 ({}) failed to shutdown".format(node1_name)]
+        return [False, "Step 7: Node 1 ({}) failed to shutdown".format(node1_name)]
     else:
         print("Master SM successfully shutdown")
 
     # Verify the other active SM goes into the master state using sminfo again.
+    # Can take up to 90 seconds (made up that number) for the sm to take over master state
     sminfo_output = node2.command("sudo sminfo -L {}".format(node2_lid))
 
     print("Waiting for node 2 ({}) to take master position. This could take up to 90 seconds...".format(node2_name))
     counter = 0
     while "SMINFO_MASTER" not in sminfo_output[0] and counter < 90:
+        # Sends sminfo command every second until the SM takes master state
         time.sleep(1)
         sminfo_output = node2.command("sudo sminfo -L {}".format(node2_lid))
         counter += 1
@@ -373,6 +375,76 @@ def test3(node1, node2, guid_list):
     return [True, "Test 3 completed successfully"]
 
 def test4(node1, node2, guid_list):
+    # Making sure whichever node is the master node is 'node1'. Also getting name, guid, and lid for easier testing
+    node1_name = node1.ethif.aliases[0]
+    node1_ibstat_output = node1.command("sudo ibstat")
+    node1_guid = str(re.search(r"Node GUID.*0x(.*)", node1_ibstat_output[0])[1]).strip()
+    node1_lid = str(re.search(r"Base lid:(.*)", node1_ibstat_output[0])[1]).strip()
+    node1_sminfo_output = node1.command("sudo sminfo -L {}".format(node1_lid))
+    node1_state = str(re.search(r"state \d(.*)", node1_sminfo_output[0])[1]).strip()
+
+    node2_name = node2.ethif.aliases[0]
+    node2_ibstat_output = node2.command("sudo ibstat")
+    node2_guid = str(re.search(r"Node GUID.*0x(.*)", node2_ibstat_output[0])[1]).strip()
+    node2_lid = str(re.search(r"Base lid:(.*)", node2_ibstat_output[0])[1]).strip()
+    node2_sminfo_output = node1.command("sudo sminfo -L {}".format(node2_lid))
+    node2_state = str(re.search(r"state \d(.*)", node2_sminfo_output[0])[1]).strip()
+
+    if node1_state == "SMINFO_MASTER" and node2_state == "SMINFO_STANDBY":
+        pass
+    elif node1_state == "SMINFO_STANDBY" and node2_state == "SMINFO_MASTER":
+        # Swapping node 1 and 2 if node 2 is the master. This just keeps the master node as 'node1'
+        node_temp = node1
+        node_temp_guid = node1_guid
+        node_temp_lid = node1_lid
+        node_temp_name = node1_name
+
+        node1 = node2
+        node1_guid = node2_guid
+        node1_lid = node2_lid
+        node1_name = node2_name
+        
+        node2 = node_temp
+        node2_guid = node_temp_guid
+        node2_lid = node_temp_lid
+        node2_name = node_temp_name
+    else:
+        return [False, "Error determining which node is master:\n{} state = {}\n{} state = {}".format(node1_name, node1_state, node2_name, node2_state)]
+
+
+    # Shutdown the standby SM.
+    print("Shutting down the standby SM ({})...".format(node2_name))
+    node2.sm.stop()
+    # Wait three seconds for SM to stop
+    time.sleep(3)
+    #Verify SM is stopped
+    if node2.sm.status() == 'active':
+        return [False, "Node 2 ({}) failed to shutdown".format(node2_name)]
+    else:
+        print("Standby SM successfully shutdown")
+
+    # Verify that the previous master SM is still the master. 
+    sminfo_output = node1.command("sudo sminfo -L {}".format(node1_lid))
+
+    if "SMINFO_STANDBY" in sminfo_output[0]:
+        print("Node 1 ({}) correctly still reporting being the master node.".format(node1_name))
+    else:
+        return [False, "Node 1 ({}) is not reporting to be the master node. Output from sminfo: {}".format(node1_name,sminfo_output)]
+
+    # Run 'saquery' on a node in the current pair
+    print("Running saquery on {}".format(node1_name))
+    saquery_output = node1.command("sudo saquery -t 5000")
+
+    # Parsing 'saquery' and adding all node guids to list
+    saquery_guid_list = re.findall(r".*node_guid.*0x(.*)", saquery_output[0])
+
+    # Verifying all nodes are present in saquery output by comparing the given guid_list to the one from saquery
+    compare_value = set(guid_list) & set(saquery_guid_list)
+    if len(compare_value) != len(guid_list):
+        return [False, "Could not verify all nodes are present in saquery on node: {}".format(node1_name)]
+    else:
+        print("All nodes were successfully verified and present in saquery output")
+
     return [True, "Test 4 completed successfully"]
 
 MasterTest = subtest.Subtest(test=masterTest, name="ib sm failover tests", number='1')
