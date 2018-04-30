a3ca5cee16cbb5769c0fcf7f6cda409de621d9ae
Kevin Pelzel <kevinpelzel@Kevins-MBP.T-mobile.com>
2018-04-29 20:10:39 -0500

    checking for master and stanby

--- interop/testlib/scripts/ib_sm_tests.py
+++ interop/testlib/scripts/ib_sm_tests.py
@@ -97,38 +97,39 @@ def test2():
 
 
 def nodePairs(node1, node2, guid_list):
-    # get node1 and node2 guid and lid
+    # get node1 and node2 guid, lid, and name
+    node1_name = node1.ethif.aliases[0]
     node1_ibstat_output = node1.command("sudo ibstat")
     node1_guid = str(re.search(r"Node GUID.*0x(.*)",
                                node1_ibstat_output[0])[1]).strip()
-    node1_lid = str(
-        re.search(r"Base lid:(.*)", node1_ibstat_output[0])[1]).strip()
+    node1_lid = str(re.search(r"Base lid:(.*)", node1_ibstat_output[0])[1]).strip()
 
+    node2_name = node2.ethif.aliases[0]
     node2_ibstat_output = node2.command("sudo ibstat")
     node2_guid = str(re.search(r"Node GUID.*0x(.*)",
                                node2_ibstat_output[0])[1]).strip()
     node2_lid = str(
         re.search(r"Base lid:(.*)", node2_ibstat_output[0])[1]).strip()
 
-    print("starting", node1.ethif.aliases[0], " subnet manager")
+    print("starting", node1_name, " subnet manager")
     # if starting fails try again up to 5 times
     counter = 0
     while not node1.sm.start() and counter < 5:
         print("{} failed to start. Trying again: {}".format(
-            node1.ethif.aliases[0], counter))
+            node1_name, counter))
         counter += 1
         if counter == 5:
-            return [False, "Subnet manager on node {} failed to start".format(node1.ethif.aliases[0])]
+            return [False, "Subnet manager on node {} failed to start".format(node1_name)]
 
     # wait 3 seconds for sm to start
-    print("Waiting for SM on {} to start...".format(node1.ethif.aliases[0]))
+    print("Waiting for SM on {} to start...".format(node1_name))
     time.sleep(1)
 
     
 
     # run "saquery" on a node in the fabric
     #output = node1.command("sudo saquery | grep \"NodeDescription\" | sed 's/.*\.\.\.//' | sed 's/\s.*$//'")
-    print("Running saquery on ", node1.ethif.aliases[0])
+    print("Running saquery on ", node1_name)
     output = node1.command("sudo saquery -t 5000")
 
     # was not getting output consistently from saquery so checking again
@@ -147,23 +148,70 @@ def nodePairs(node1, node2, guid_list):
     # Verifying all nodes are present in saquery output
     compare_value = set(guid_list) & set(saquery_guid_list)
     if len(compare_value) != len(guid_list):
-        return [False, "Could not verify all nodes are present in saquery on node: {}".format(node1.ethif.aliases[0])]
+        return [False, "Could not verify all nodes are present in saquery on node: {}".format(node1_name)]
     else:
         print("Nodes were successfully verified and existing")
 
-    print("running sminfo on {} to check if it's master".format(node1.ethif.aliases[0]))
+    print("running sminfo on {} to check if it's master".format(node1_name))
     # using sminfo, verify that the running SM is the master
     sminfo_output = node1.command("sudo sminfo -L {}".format(node1_lid))
 
     if "SMINFO_MASTER" in sminfo_output[0]:
-        print("Node1 ({}) correctly reported being the master node".format(node1.ethif.aliases[0]))
+        print("Node1 ({}) correctly reported being the master node".format(node1_name))
     else:
-        return [False, "Node1({}) is not reporting to be the master node. Output from sminfo: {}".format(node1.ethif.aliases[0],sminfo_output)]
+        return [False, "Node1({}) is not reporting to be the master node. Output from sminfo: {}".format(node1_name,sminfo_output)]
 
-
-    return [True, "Node pair successfully tested"]
 
     # Start a SM on the second machine in the current pair
+    print("Starting SM on node 2: {}".format(node2_name))
+    node2.sm.start()
+    time.sleep(1)
+
+    # Verify that the SMs behave according to the SM priority rules. Use "# ibdiagnet -r" again.
+    # a. SM with highest numerical priority value is master and the other is in standby. 
+    # b. If both SMs have the same priority value then the SM with the smallest guid is master and
+    # the other is in standby.
+
+    node1_sminfo_output = node1.command("sudo sminfo -L {}".format(node1_lid))
+    node1_priority = str(re.search(r"priority (\d*)", node1_sminfo_output[0])[1])
+    node1_state = str(re.search(r"state \d(.*)", node1_sminfo_output[0])[1]).strip()
+
+    node2_sminfo_output = node2.command("sudo sminfo -L {}".format(node2_lid))
+    node2_priority = str(re.search(r"priority (\d*)", node2_sminfo_output[0])[1])
+    node2_state = str(re.search(r"state \d(.*)", node2_sminfo_output[0])[1]).strip()
+
+
+    if node1_priority > node2_priority:
+        if node1_state == "SMINFO_MASTER" and node2_state == "SMINFO_STANDBY":
+            print("{} and {} reporting the correct states ({} and {})".format(node1_name, node2_name, node1_state, node2_state))
+        else:
+            return [False, "Some nodes not reporting the correct state. Reported states:\n {}: state = {} and priority = {}\n {}: state = {} and priority = {}".format(node1_name, node1_state, node1_priority, node2_name, node2_state, node2_priority)]
+    elif node1_priority < node2_priority:
+        if node2_state == "SMINFO_MASTER" and node1_state == "SMINFO_STANDBY":
+            print("{} and {} reporting the correct states ({} and {})".format(node2_name, node1_name, node2_state, node1_state))
+        else:
+            return [False, "Some nodes not reporting the correct state. Reported states:\n {}: state = {} and priority = {}\n {}: state = {} and priority = {}".format(node1_name, node1_state, node1_priority, node2_name, node2_state, node2_priority)]
+    elif node1_priority == node2_priority:
+        node1_guid_decimal = int(node1_guid, 16)
+        node2_guid_decimal = int(node2_guid, 16)
+        if node1_guid_decimal < node2_guid_decimal:
+            if node1_state == "SMINFO_MASTER" and node2_state == "SMINFO_STANDBY":
+                print("{} and {} reporting the correct states ({} and {})".format(node1_name, node2_name, node1_state, node2_state))
+            else:
+                return [False, "Some nodes not reporting the correct state. Reported states:\n {}: state = {} and priority = {}\n {}: state = {} and priority = {}".format(node1_name, node1_state, node1_priority, node2_name, node2_state, node2_priority)]
+        elif node1_guid_decimal > node2_guid_decimal:
+            if node2_state == "SMINFO_MASTER" and node1_state == "SMINFO_STANDBY":
+                print("{} and {} reporting the correct states ({} and {})".format(node2_name, node1_name, node2_state, node1_state))
+            else:
+                return [False, "Some nodes not reporting the correct state. Reported states:\n {}: state = {} and priority = {}\n {}: state = {} and priority = {}".format(node1_name, node1_state, node1_priority, node2_name, node2_state, node2_priority)]
+        else:
+            return [False, "Guid decimal calculations failed:\n {} guid decimal = {}\n{} guid decimal = {}".format(node1_name, node1_guid_decimal, node2_name, node2_guid_decimal)]
+    else:
+        return [False, "Node priority was not detected correctly:\n {} priority = {}\n{} priority = {}".format(node1_name, node1_priority, node2_name, node2_priority)]
+
+        
+
+
 
 Test1 = subtest.Subtest(test=test1, name="ib sm subtest 1", number='1')
 Test2 = subtest.Subtest(test=test2, name="ib sm subtest2", number='2')
