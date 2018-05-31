0c856cc7484e100ba159adb8e8598f80400a4ff8
Aaron Overby <overbya0300@my.uwstout.edu>
2018-05-31 09:47:44 -0600

    bulk of ping test code for step A finished, still need to clean up and organize output, making sure this gets committed because I keep getting kicked off the cluster

--- interop/testlib/scripts/sample_tests.py
+++ interop/testlib/scripts/sample_tests.py
@@ -89,11 +89,79 @@ def IBFabricInit():
 
         else:
             print("{} is not currently active, running tests on other active nodes".format(node.ethif.id))
-
+    for node in network.nodes:
+        node.sm.stop()
     if all(nodeSuccess) == True:
         return [True, comments]
     else:
         return [False, comments]
+
+
+#function for removing the destination node's ib0 address from sending nodes
+#arp table then pinging with specified options and packet size
+def pingTests(node, size, ibipAddr):
+    node.command("ip neigh flush {}".format(ibipAddr))
+    node.command("ping -i 0.2 -t 3 -c 10 -s {} {}".format(size, ibipAddr))
+
+def PingNodesStepA
+    for nodeSRC in network.nodes:
+        nodeSRC.sm.start()
+        time.sleep(2)
+        ibAddrSRC = nodeSRC.ibif.id
+
+        #verifying the SM started is master 
+        isMaster = nodeSRC.command("sminfo")[0].split("_")
+        master = isMaster[-1]
+        if master == "MASTER":
+
+            #pinging all nodes with specified packet sizes as long as the ib
+            #interface is not the same as the source node
+            for nodeDEST in network.node:
+                if nodeDEST.ibif.id != ibAddrSRC:
+                    for size in packetSize:
+                    pingTests(nodeSRC, packetSize, nodeDEST.ibif.id)
+        else:
+            smNotMaster = "The Subnet Manager being used did not successfully become master."
+            return(smNotMaster)
+
+def IB_IPoIBConnectModeStepA():
+
+    packetSize = [511,1025,2044,8192,32768,65492]
+
+    #function for removing the destination node's ib0 address from sending nodes
+    #arp table then pinging with specified options and packet size
+    def pingTests(node, size, ibipAddr):
+        node.command("ip neigh flush {}".format(ibipAddr))
+        node.command("ping -i 0.2 -t 3 -c 10 -s {} {}".format(size, ibipAddr))
+
+    #setting the mode to connected on all nodes and verifying
+    for node in network.nodes:
+        node.command("echo 'connected' > /sys/class/net/ib0/mode")
+    for node in network.nodes:
+        if node.command("grep -e 'datagram' -e 'connected' /sys/class/net/ib0/mode")[0] == 'connected':
+            print("{} is in connected mode\n".format(node.ethif.id))
+        else:
+            print("{} is not in connected mode\n".format(node.ethif.id))
+
+    #stopping all sm's in the system, then verifying they are inactive
+    for node in network.nodes:
+        node.sm.stop();
+    for node in network.nodes:
+        if node.sm.status() == "inactive":
+            continue
+        else:
+            return("{}'s sm has not been shut off, please restart test.".format(node.ethif.id))
+
+    #verifying all nodes are up before continuing with tests
+    for node in network.nodes:
+        if node.is_up():
+            continue
+        else:
+            return("{} is down, please restart and try the test again.".format(node.ethif.id))
+
+    PingNodesStepA()
+
+
 
 
 def sample_test1():
@@ -110,7 +178,9 @@ def sample_test2():
 
 test = subtest.Subtest(test=IBFabricInit, name="IB Fabric Initialization", number='1')
 IBFabricInit = Test.Test(tests=[test], description="Currently tests the fabric initialization of the master node using opensm as subnet manager")
+IPoIBCMpings = subtest.Subtest(test=IB_IPoIBConnectModeStepA, name = "Step A Connect Mode ping tests", number = '1')
 
+IB_IPoIBConnectMode = Test.Test(tests=[test], description= "Pings all nodes from each other node after flushing the destination ib0 address from sending nodes arp table")
 sample1 = subtest.Subtest(test=sample_test2, name="Sample test 2", number='2')
 sample2 = subtest.Subtest(test=sample_test1, name="Sample test 1", number='1')
 sampleTest1 = Test.Test(tests=[sample1,sample2],  description="this is the sample_test1 description", group=["latency_tests","bandwidth_tests"])
